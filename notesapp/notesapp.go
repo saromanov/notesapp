966d1d04f6b1d6a213e23784317e968a48877bed
Sergey Romanov <xxsmotur@gmail.com>
2015-12-18 13:18:40 +0500

    Update

--- notesapp/notesapp.go
+++ notesapp/notesapp.go
@@ -3,17 +3,17 @@ package notesapp
 import (
 	"fmt"
 	//"log"
-	"net/http"
+	//"net/http"
 
 	"github.com/beatrichartz/martini-sockets"
 	"github.com/go-martini/martini"
-	//"github.com/googollee/go-socket.io"
 	"github.com/martini-contrib/render"
 )
 
 type Note struct {
-	Title    string `json:"title"`
-	NoteItem string `json: "note_item"`
+	Event string `json:"event"`
+	Title string `json:"title"`
+	Text  string `json: "text"`
 }
 
 type Client struct {
@@ -26,23 +26,6 @@ type Client struct {
 }
 
 func Start() {
-
-	/*server, err := socketio.NewServer(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	server.On("connection", func(so socketio.Socket) {
-
-		so.On("newnote", func(msg string) {
-			fmt.Println("NEW: ", msg)
-		})
-	})
-
-	server.On("error", func(so socketio.Socket, err error) {
-		log.Println("error:", err)
-	})*/
-
 	m := martini.Classic()
 
 	// Use Renderer
@@ -55,19 +38,13 @@ func Start() {
 		r.HTML(200, "index", "")
 	})
 
-	m.Get("/:id", func(w http.ResponseWriter, r *http.Request, params martini.Params) {
-		w.Header().Set("Content-Type", "appication/json")
-		w.WriteHeader(200)
-	})
-
 	// render the room
 	m.Get("/list", func(r render.Render, params martini.Params) {
 		r.HTML(200, "room", map[string]map[string]string{"room": map[string]string{"name": params["name"]}})
 	})
 
-	m.Get("/socket.io/", sockets.JSON(Note{}), func(r render.Render, params martini.Params, receiver <-chan *Note, sender chan<- *Note, done <-chan bool, disconnect chan<- int, err <-chan error) (int, string) {
+	m.Get("/sockets/:id", sockets.JSON(Note{}), func(r render.Render, params martini.Params, receiver <-chan *Note, sender chan<- *Note, done <-chan bool, disconnect chan<- int, err <-chan error) (int, string) {
 		client := &Client{params["id"], receiver, sender, done, err, disconnect}
-		fmt.Println("AAAAAAAAAAA")
 		// A single select can be used to do all the messaging
 		for {
 			select {
--- notesapp/public/js/app.js
+++ notesapp/public/js/app.js
@@ -1,28 +1,38 @@
 /** @jsx React.DOM */
-var socket = io.connect();
 
-var NoteItem = React.createClass({
 
-	getInitialState: function() {
-    	return {value: ''};
-  	},
+var ws = new WebSocket("ws://" + location.host + "/sockets/"+ name);
 
-	/**
-	 * @return {object}
-	*/
-	render: function(){
-		var value = this.state.value
+var NoteStore = {
+	notes: [],
+	addNote: function(note) {
+		this.notes.push(note);
+	}
+};
+
+var NoteList = React.createClass({
+	render : function(){
 		return (
-			  <div className="note">
-      			  <input type="text" id="note-title" size="50" ref="title" placeholder="Enter title of note"/> <br />
-      			  <textarea id="note-text" ref="notetext" rows="10" cols="50" value={value} onChange={this._onChange}></textarea><br />
-    			  <button id ='add' className='btn btn-primary btn-lg' onClick={this._onAddNote}>Save</button><br />
-  			 </div>
-		)
+			<strong> Back </strong>
+		);
+	}
+});
+
+var EnterNote = React.createClass({
+
+	getInitialState(){
+		return {value: ''};
 	},
 
-	_handleData: function(event) {
-		console.log(event);
+	render: function(){
+		var value = this.state.value;
+		return (
+			 <div className="note">
+      			  <input type="text" id="note-title" size="40" ref="title" placeholder="Enter title of note"/> <br />
+      			  <textarea id="note-text" ref="notetext" rows="10" cols="40" value={value} onChange={this._onChange}></textarea><br />
+    			  <button id ='add' className='btn btn-primary btn-lg' onClick={this._onAddNote}>Save</button><br />
+  			 </div>
+		);
 	},
 
 	_onAddNote: function(event) {
@@ -30,7 +40,8 @@ var NoteItem = React.createClass({
 		var title = this.refs['title'].value;
 		var text = this.refs['notetext'].value;
 		var timestamp = Date.now();
-		this.socket.emit('newnote', {'title': title, 'text': text})
+		this.props.store.addNote({'title': title, 'text': text});
+		ws.send(JSON.stringify({'event': 'add', 'title': title, 'text': text}));
 	},
 
 	_onChange: function(event){
@@ -39,7 +50,46 @@ var NoteItem = React.createClass({
 		})
 	},
 });
+
+var NoteApp = React.createClass({
+
+	getInitialState: function() {
+		var name = "test";
+		ws.addEventListener("message", function(e) {
+      		this.ws.send(JSON.parse(e.data), 'right');
+    	});
+    	return {value: ''};
+  	},
+
+	/**
+	 * @return {object}
+	*/
+	render: function(){
+		var value = this.state.value;
+
+		items  = (
+			  <ul className="noteItems">
+			      <strong> Value </strong>
+			   </ul>
+			);
+		return (
+			<div>
+			 <EnterNote 
+			     store={this.props.store}
+			     onChange={this._onChange.bind(this, value)} />
+
+			 <NoteList />
+
+			</div>
+		)
+	},
+
+	_onChange: function(){
+		console.log("Change");
+	}
+});
+
 ReactDOM.render(
-  <NoteItem />,
+  <NoteApp store={NoteStore}/>,
   document.getElementById('note')
 );
